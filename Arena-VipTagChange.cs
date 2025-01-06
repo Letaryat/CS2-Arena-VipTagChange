b20ec3b0611aa44fe6bafa232ca8d8305d715c4e
Letaryat <69041891+Letaryat@users.noreply.github.com>
2025-01-06 19:01:07 +0100

    0.0.3
    Dziala:
    - Wylaczenie / Wlaczenie tagu
    - Kolor Tagow
    - Kolor Nickow
    - Kolor Czatu
    Do zrobienia:
    - Baza danych
    - Permisje aby wyswietlalo tag jezeli ma vipa
    - Chyba pomniejsze bledy np z wylaczaniem sie menu ?

--- Arena-VipTagChange.cs
+++ Arena-VipTagChange.cs
@@ -29,7 +29,7 @@ public class Arena_VipTagChange : BasePlugin
     public static PluginCapability<ITagApi> Capability_TagsApi = new("tags:api");
     public static ITagApi? SharedApi_Tag { get; private set; }
     private IMenuApi? _api;
-    private readonly PluginCapability<IMenuApi?> _pluginCapability = new("menu:nfcore");    
+    private readonly PluginCapability<IMenuApi?> _pluginCapability = new("menu:nfcore");
 
     List<string> Colors = [
         "Default",
@@ -57,14 +57,14 @@ public class Arena_VipTagChange : BasePlugin
     public override void Load(bool hotReload)
     {
         Logger.LogInformation("Arena_VipTagChange - Loaded");
-        if(SharedApi_Tag is null)
+        if (SharedApi_Tag is null)
         {
             PluginCapability<ITagApi> Capability_TagApi = new("tags:api");
             SharedApi_Tag = Capability_TagApi.Get();
         }
 
     }
-    public override void Unload(bool hotReload) 
+    public override void Unload(bool hotReload)
     {
         Logger.LogInformation("Arena_VipTagChange - Unloaded");
     }
@@ -99,7 +99,7 @@ public class Arena_VipTagChange : BasePlugin
 
     public string? GetApi(CCSPlayerController controller)
     {
-        if(controller == null) { return null;}
+        if (controller == null) { return null; }
         return SharedApi_Tag?.GetPlayerTag(controller, Tags.Tags_Tags.ChatTag);
     }
 
@@ -113,51 +113,85 @@ public class Arena_VipTagChange : BasePlugin
         var ArenaName = GetPlayerArenaTag(player!);
         var arg = commandInfo.GetArg(1);
         var test = GetApi(player!);
-        var newtag = $"[{arg}] {ArenaName} |";
-        if(player == null){
+        var newtag = $"{arg} {ArenaName} |";
+        if (player == null)
+        {
             Server.PrintToChatAll("player null");
         }
         Server.PrintToChatAll($"{player!.PlayerName} - {ArenaName} - new tag: {arg} - Api: {test}");
         SharedApi_Tag?.SetPlayerTag(player, Tags.Tags_Tags.ScoreTag, newtag);
         SharedApi_Tag?.SetPlayerTag(player, Tags.Tags_Tags.ChatTag, $"[{arg}]");
-        SharedApi_Tag?.SetPlayerColor(player, Tags.Tags_Colors.NameColor, "{Blue}");
-        SharedApi_Tag?.SetPlayerColor(player, Tags.Tags_Colors.ChatColor, "{DarkRed}");
+        //SharedApi_Tag?.SetPlayerColor(player, Tags.Tags_Colors.NameColor, "{Blue}");
+        //SharedApi_Tag?.SetPlayerColor(player, Tags.Tags_Colors.ChatColor, "{DarkRed}");
 
     }
 
     [ConsoleCommand("css_menu", "Ability for VIP to change their Scoreboard and Chat tag")]
     public void TestMenu(CCSPlayerController? player, CommandInfo commandInfo)
     {
-        if(player == null){ return; }
-        var menu = _api?.NewMenu("Test");
-        menu?.AddMenuOption($"Toggle Tag - {SharedApi_Tag?.GetPlayerToggleTags(player)}", (player, option) => {
+        if (player == null) { return; }
+        var menu = _api?.NewMenu("Tag menu");
+        menu?.AddMenuOption($"Toggle Tag - {SharedApi_Tag?.GetPlayerToggleTags(player)}", (player, option) =>
+        {
             SharedApi_Tag?.SetPlayerToggleTags(player, !SharedApi_Tag.GetPlayerToggleTags(player));
             CounterStrikeSharp.API.Modules.Menu.MenuManager.CloseActiveMenu(player);
         });
-        menu?.AddMenuOption("Tag Color", (player, option) => {
-            NoweMenu(player);
+        menu?.AddMenuOption("Tag Color", (player, option) =>
+        {
+            CreateMenu(player, 1);
             CounterStrikeSharp.API.Modules.Menu.MenuManager.CloseActiveMenu(player);
         });
-        menu?.AddMenuOption("Chat Color", (player, option) => {
-            NoweMenu(player);
+        menu?.AddMenuOption("Chat Color", (player, option) =>
+        {
+            CreateMenu(player, 2);
             CounterStrikeSharp.API.Modules.Menu.MenuManager.CloseActiveMenu(player);
         });
-        menu?.AddMenuOption("Name Color", (player, option) => {
-            NoweMenu(player);
+        menu?.AddMenuOption("Name Color", (player, option) =>
+        {
+            CreateMenu(player, 3);
             CounterStrikeSharp.API.Modules.Menu.MenuManager.CloseActiveMenu(player);
         });
         menu?.Open(player);
     }
 
-    public void NoweMenu(CCSPlayerController? player)
+    public void CreateMenu(CCSPlayerController? player, int type)
     {
-        if(player == null){ return; }
-        var menu = _api?.NewMenu("Test drugie menu");
-
-        foreach(var chatcolors in Colors)
+        if (player == null) { return; }
+        var menu = _api?.NewMenu("Test 2");
+        switch (type)
         {
-            menu?.AddMenuOption($"{chatcolors}", (player, option) => {
-                SharedApi_Tag?.SetPlayerColor(player, Tags.Tags_Colors.ChatColor, $"{{{chatcolors}}}");
+            case 1:
+                menu = _api?.NewMenu("Tag color");
+                break;
+            case 2:
+                menu = _api?.NewMenu("Chat Color");
+                break;
+            case 3:
+                menu = _api?.NewMenu("Name Color");
+                break;
+        }
+
+        foreach (var chatcolors in Colors)
+        {
+            menu?.AddMenuOption($"{chatcolors}", (player, option) =>
+            {
+                switch (type)
+                {
+                    case 1:
+                        string? playertag = SharedApi_Tag?.GetPlayerTag(player, Tags.Tags_Tags.ScoreTag);
+                        var splittedTag = playertag!.Split("ARENA")[0];
+
+                        SharedApi_Tag?.SetPlayerTag(player, Tags.Tags_Tags.ChatTag, $"{{{chatcolors}}}{splittedTag}");
+
+                        break;
+                    case 2:
+                        SharedApi_Tag?.SetPlayerColor(player, Tags.Tags_Colors.ChatColor, $"{{{chatcolors}}}");
+                        break;
+                    case 3:
+                        SharedApi_Tag?.SetPlayerColor(player, Tags.Tags_Colors.NameColor, $"{{{chatcolors}}}");
+                        break;
+                }
+                //SharedApi_Tag?.SetPlayerColor(player, Tags.Tags_Colors.ChatColor, $"{{{chatcolors}}}");
                 CounterStrikeSharp.API.Modules.Menu.MenuManager.CloseActiveMenu(player);
                 string message = $"Your new color: {{{chatcolors}}}{chatcolors}";
                 player.PrintToChat($"{message.ReplaceColorTags()}");
